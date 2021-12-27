18ebfeed90c8f948a4ca859eef77df39c4f2c40d
Brandon <46331018+jbiams77@users.noreply.github.com>
2021-12-26 17:36:26 -0800

    transferring from DMA to queue in ISR

--- src/main.cpp
+++ src/main.cpp
@@ -29,13 +29,16 @@ to ticks using the portTICK_PERIOD_MS constant. */
 /* The number of items the queue can hold.  This is 1 as the receive task
 will remove items as they are added, meaning the send task should always find
 the queue empty. */
-#define mainQUEUE_LENGTH                    ( 10 )
-
+#define mainQUEUE_LENGTH       ( 10 )
+#define UART_RX__SZ            ( 64 )
 /*-----------------------------------------------------------*/
+
 typedef struct Message {
-  char body[20];
+  uint8_t body[UART_RX__SZ];
   int count;
 } Message;
+
+uint8_t rx_buffer[UART_RX__SZ];
 
 /*
  * The tasks as described in the comments at the top of this file.
@@ -45,6 +48,8 @@ static void prvQueueSendTask( void *pvParameters );
 
 /*-----------------------------------------------------------*/
 
+// TODO: Move these to class when working
+void UART_DMA_Init();
 
 /* The queue used by both tasks. */
 static QueueHandle_t xQueue = NULL;
@@ -63,6 +68,7 @@ int main(void)
     MX_I2C2_Init();
     MX_USART1_UART_Init(&huart1);
 
+    UART_DMA_Init();
     /* Create the queue. */
     xQueue = xQueueCreate( mainQUEUE_LENGTH, sizeof( Message ) );
 
@@ -70,12 +76,12 @@ int main(void)
     {
         /* Start the two tasks as described in the comments at the top of this
         file. */
-        xTaskCreate(prvQueueSendTask,
-                    "TX",
-                    configMINIMAL_STACK_SIZE,
-                    NULL,
-                    mainQUEUE_SEND_TASK_PRIORITY,
-                    NULL );
+        // xTaskCreate(prvQueueSendTask,
+        //             "TX",
+        //             configMINIMAL_STACK_SIZE,
+        //             NULL,
+        //             mainQUEUE_SEND_TASK_PRIORITY,
+        //             NULL );
 
         xTaskCreate( prvQueueReceiveTask,               /* The function that implements the task. */
                     "Rx",                               /* The text name assigned to the task - for debug only as it is not used by the kernel. */
@@ -95,34 +101,6 @@ int main(void)
     }
     /* USER CODE END 3 */
 }
-static void prvQueueSendTask( void *pvParameters )
-{
-TickType_t xNextWakeTime;
-const unsigned long ulValueToSend = 100UL;
-
-    /* Remove compiler warning about unused parameter. */
-    ( void ) pvParameters;
-    /* Initialise xNextWakeTime - this only needs to be done once. */
-    xNextWakeTime = xTaskGetTickCount();
-    Message msg;
-    msg.count = 0;
-    while (1)
-    {
-        /* Place this task in the blocked state until it is time to run again. */
-        vTaskDelayUntil( &xNextWakeTime, mainQUEUE_SEND_FREQUENCY_MS );
-        
-        strcpy(msg.body, "Hello-World");
-        msg.count += 1;
-        /* Send to the queue - causing the queue receive task to unblock and
-        toggle the LED.  0 is used as the block time so the sending operation
-        will not block - it shouldn't need to block as the queue should always
-        be empty at this point in the code. */
-        if (xQueueSend( xQueue, &msg, 10) != pdTRUE)
-        {
-          Error_Handler();
-        }
-    }
-}
 
 /*-----------------------------------------------------------*/
 
@@ -133,16 +111,49 @@ const unsigned long ulExpectedValue = 100UL;
 
     /* Remove compiler warning about unused parameter. */
     ( void ) pvParameters;
-    Message rcv_msg;
-    volatile int x = 0;
-    volatile char *body;
+    static Message rcv_msg;
     while (1)
     {
         /*  To get here something must have been received from the queue, but
         is it the expected value?  If it is, toggle the LED. */
-        if(xQueueReceive(xQueue, &rcv_msg, 1) == pdTRUE)
+        if(xQueueReceive(xQueue, &rcv_msg, portMAX_DELAY) == pdTRUE)
         {
           HAL_GPIO_TogglePin(GREEN_LED_GPIO_Port, GREEN_LED_Pin);
         }
     }
+}
+
+void UART_DMA_Init()
+{ 
+  // set data direction pin is low
+  HAL_GPIO_WritePin(DATA_DIR_GPIO_Port, DATA_DIR_Pin, GPIO_PIN_RESET);
+
+  // turn on the UART RX port
+  if (HAL_UARTEx_ReceiveToIdle_DMA(&huart1, rx_buffer, UART_RX__SZ) != HAL_OK) {
+    Error_Handler();
+  }
+
+}
+
+void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size)
+{
+  Message msg;
+  memcpy(msg.body, rx_buffer, UART_RX__SZ); // Copy from DMA (rx_buffer) to msg
+  msg.count = 0;
+  
+  /* xQueueSendFromISR() will set *pxHigherPriorityTaskWoken to pdTRUE if sending
+  to the queue caused a task to unblock, and the unblocked task has a priority higher
+  than the currently running task. If xQueueSendFromISR() sets this value to pdTRUE
+  then a context switch should be requested before the interrupt is exited. */
+  BaseType_t xHigherPriorityTaskWoken;
+  xHigherPriorityTaskWoken =pdFALSE;
+  /* Send to the queue - causing the queue receive task to unblock and
+  toggle the LED.  0 is used as the block time so the sending operation
+  will not block - it shouldn't need to block as the queue should always
+  be empty at this point in the code. */
+  if (xQueueSendFromISR(xQueue, &msg, &xHigherPriorityTaskWoken) != pdTRUE)
+  {
+    Error_Handler();
+  }
+
 }
